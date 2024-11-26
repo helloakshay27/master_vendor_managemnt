cce116bd9e627948b87a6deb5a9e4760f86b0bcf
Akshay Shinde <akshay.shinde@lockated.com>
2024-11-26 15:30:09 +0530

    index.html

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# Master Vendor Management
-This is the README file for the Master Vendor Management project.
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Exposes server to the network (0.0.0.0)
+    port: 5173, // Use a non-privileged port
+    strictPort: true, // Fail if port 3000 is not available
+    proxy: {
+      '/api': {
+        target: 'https://marathon.lockated.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
